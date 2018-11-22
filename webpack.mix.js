53db6018b14c2c17f743392d8d19c2eb791a1fdc
Ilya <ilia.nikulin@gmail.com>
2018-11-22 21:13:09 +0300

    list, purgecss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
+const glob = require("glob-all");
+const PurgecssPlugin = require("purgecss-webpack-plugin");
 
 /*
  |--------------------------------------------------------------------------
@@ -12,12 +14,44 @@ const tailwindcss = require('tailwindcss');
  |
  */
 
-/*
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
-*/
+// Custom PurgeCSS extractor for Tailwind that allows special characters in
+// class names.
+//
+// https://github.com/FullHuman/purgecss#extractor
+class TailwindExtractor {
+    static extract(content) {
+        return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+    }
+}
 
-mix.postCss('resources/css/wp-pusher.css', 'public/css', [
+mix.postCss('resources/css/list.css', 'public/css', [
     tailwindcss('./tailwind.js'),
 ])
-.browserSync('lar-tailwind/wp-pusher');
+// .browserSync('lar-tailwind/wp-pusher');
+
+// Only run PurgeCSS during production builds for faster development builds
+// and so you still have the full set of utilities available during
+// development.
+if (mix.inProduction()) {
+    mix.webpackConfig({
+        plugins: [
+            new PurgecssPlugin({
+
+                // Specify the locations of any files you want to scan for class names.
+                paths: glob.sync([
+                    path.join(__dirname, "resources/views/**/*.blade.php"),
+                    path.join(__dirname, "resources/js/**/*.vue")
+                ]),
+                extractors: [
+                    {
+                        extractor: TailwindExtractor,
+
+                        // Specify the file extensions to include when scanning for
+                        // class names.
+                        extensions: ["html", "js", "php", "vue"]
+                    }
+                ]
+            })
+        ]
+    });
+}
